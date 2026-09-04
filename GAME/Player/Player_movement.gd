bfda2e15b6626d49fade0f900b4aa2ec09fe8ff6
Ethan Grané G <unitycodetrigger@gmail.com>
2026-09-04 20:18:42 +0200

    Player jump solved

--- GAME/Player/Player_movement.gd
+++ GAME/Player/Player_movement.gd
@@ -16,17 +16,21 @@ var n_jumps := 0
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += to_get_gravity()
-	if Input.is_action_pressed("Salto") and coyote_timer > 0.0 and n_jumps == 0:
-		velocity.y = JUMP_VELOCITY
+	velocity.y += to_get_gravity() * delta
+	
+	if Input.is_action_just_pressed("Salto") and coyote_timer > 0.0 and n_jumps == 0 and velocity.y > 10:
+		jump()
 		n_jumps += 1
+		
 	if Input.is_action_just_released("Salto") and not is_on_floor():
 		velocity.y *= jump_cut_multiplayer
+		
 	if is_on_floor():
 		n_jumps = 0
 		coyote_timer = COYOTE_TIME
 	else:
 		coyote_timer -= delta
+		
 	var direction := signf(Input.get_axis("Left","Right"))
 	if direction:
 		velocity.x = direction * speed
@@ -35,18 +39,23 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func calculate_jump_speed(height : float, time_to_peak: float) -> float:
-	return (-2.0 * height / time_to_peak)
+func calculate_jump_speed(height: float, time_to_peak: float) -> float:
+	return -(2.0 * height / time_to_peak)
 
-func calculate_jump_gravity(height : float, time_to_peak: float) -> float:
-	return (2.0 * height / pow(time_to_peak,2.0))
+func calculate_jump_gravity(height: float, time_to_peak: float) -> float:
+	return (2.0 * height) / pow(time_to_peak, 2.0)
 
-func calculate_fall_gravity(height : float, time_to_descent: float) -> float:
-	return (2.0 * height / pow(time_to_descent,2.0))
-	
+func calculate_fall_gravity(height: float, time_to_descent: float) -> float:
+	return (2.0 * height) / pow(time_to_descent, 2.0)
+
 func to_get_gravity() -> float:
-	return jump_gravity if velocity.y < 0.0 else fall_gravity
-	
+	if velocity.y < 0.0:
+		return jump_gravity
+	return fall_gravity
+
+
+func jump() -> void:
+	velocity.y = jump_speed
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy"):
