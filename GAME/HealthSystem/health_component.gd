45ce12d204b84e4549da53daae58cdeea23bcc87
Ethan Grané G <unitycodetrigger@gmail.com>
2026-09-04 17:36:07 +0200

    Add HealthComponent script and scene
    Introduce HealthComponent (Node2D) with max_health, current_health, on_health_changed signal and take_damage (uses clampi). Adds health_component.gd, its UID resource, and a tscn wrapper. Also cleans up TEST.tscn by removing an unused icon texture and node.

---
+++ GAME/HealthSystem/health_component.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name HealthComponent
+
+@export var max_health : int = 3	# es un int porq se mide por hits (1 de vida = 1 hit)
+var current_health
+
+signal on_health_changed(current_health: int)
+
+func _ready() -> void:
+	current_health = max_health
+
+func take_damage(amount : int) -> void:
+	current_health = clampi(current_health - amount, 0, max_health)	#clamp capa los valores, en este caso entre 0 y vida maxima
+	on_health_changed.emit(current_health)
