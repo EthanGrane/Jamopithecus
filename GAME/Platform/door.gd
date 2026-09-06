5782b38941485c2fb96791ba0e6383e31c662bc1
chara2121 <ciriartecano@gmail.com>
2026-09-07 01:14:08 +0200

    Final_day

--- GAME/Platform/door.gd
+++ GAME/Platform/door.gd
@@ -1,9 +1,9 @@
 extends StaticBody2D
 
-
+@export var Door_position : Vector2 = Vector2(0.0,0.0)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$Area2D.position += Door_position
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	$Area2D.queue_free()
